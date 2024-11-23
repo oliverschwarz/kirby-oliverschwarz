c0958ca43f664b657ee53cf43f3251097e8fa686
oliverschwarz <oliver.schwarz@gmail.com>
2024-11-23 15:14:31 +0100

    Getting main pages back to basics for v2.

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,4 +1,11 @@
-<?php snippet('meta'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minimal Tech Blog</title>
+</head>
+<body>
 
   <header class="content">
     <h1><?php echo $page->title()->html() ?></h1>
@@ -10,4 +17,5 @@
     </article>
   </main>
 
-<?php snippet('footer'); ?>
+</body>
+</html>
