a0433bd72d7f7a83fab6e41052b7fac8f0662324
oliverschwarz <oliver.schwarz@gmail.com>
2024-11-24 10:32:26 +0100

    Corrected link to archive page with tag as parameter

--- site/snippets/tagline.php
+++ site/snippets/tagline.php
@@ -1,5 +1,5 @@
 <?php if($page->tags()->isNotEmpty()): ?>
    <span class="tagline"> / <?php echo implode(', ', array_map(function($tag) {
-    return '<a href="/tag:' . $tag . '">' . $tag . '</a>';
+    return '<a href="/archive?tag=' . $tag . '">' . $tag . '</a>';
   }, $page->tags()->split())); ?></span>
 <?php endif ?>
