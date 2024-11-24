be966066f21f2c43d24fa82c163bc3c1cea1f378
oliverschwarz <oliver.schwarz@gmail.com>
2024-11-24 18:04:55 +0100

    Fix: Correct title is now displayed on the website

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,7 +4,7 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minimal Tech Blog</title>
+    <title><?= $site->title()->html() ?></title>
 
     <meta name="referrer" content="no-referrer">
     <link rel="canonical" href="https://oliverschwarz.info">
