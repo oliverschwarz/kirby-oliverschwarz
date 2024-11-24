fdf34e5a1866e969a881410e27050b08ca5c4f93
oliverschwarz <oliver.schwarz@gmail.com>
2024-11-24 10:31:53 +0100

    Introducing the archive: Blueprint and template. The archive can be filtered by tags.

---
+++ site/templates/archive.php
@@ -0,0 +1,47 @@
+<?php snippet('header'); ?>
+
+<div class="container">
+
+<?php snippet('navigation'); ?>
+
+<main class="archive">
+<?php
+    $articles = page('articles')->children()->listed()->sortBy('date', 'desc');
+
+    if($tag = get('tag')) {
+
+        $articles = $articles->filterBy('tags', '*=', $tag);  // Changed this line
+    }
+
+    $articles = $articles->paginate(10);
+    $pagination = $articles->pagination();
+    ?>
+
+    <h1><?= $page->title() ?><?php if($tag): ?> tagged <?= html($tag) ?><?php endif ?></h1>
+
+    <div class="archive-list">
+        <?php foreach($articles as $article): ?>
+        <article>
+            <h2><a href="<?= $article->url() ?>"><?= $article->title() ?></a></h2>
+            <time datetime="<?= $article->date()->toDate('c') ?>" class="date" pubdate><?= $article->date()->toDate('F d, Y') ?></time>
+            <?php snippet('tagline', ['page' => $article]) ?>
+            <p><?= $article->text()->excerpt(150) ?></p>
+        </article>
+        <?php endforeach ?>
+    </div>
+
+    <?php if($pagination->hasPages()): ?>
+    <nav class="pagination">
+    <span>Pages: </span>
+    <?php for($i = 1; $i <= $pagination->pages(); $i++): ?>
+        <?php if($i > 1): ?>, <?php endif ?>
+        <a <?= $pagination->page() === $i ? 'class="active"' : '' ?> href="<?= $pagination->pageUrl($i) ?>"><?= $i ?></a>
+    <?php endfor ?>
+    </nav>
+    <?php endif ?>
+
+</main>
+
+</div><!-- /container -->
+
+<?php snippet('footer') ?>
